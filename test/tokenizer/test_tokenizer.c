52fb6c3541d1a42e23f56ea95594f35991e93c08
Luis <luiscristiangrigore11@gmail.com>
2025-02-23 12:38:36 +0100

    hay que mirar los valores que se pasan a token factory en tokenizer porque son incorrectos

--- test/tokenizer/test_tokenizer.c
+++ test/tokenizer/test_tokenizer.c
@@ -2,18 +2,28 @@
 #include "tokenizer.h"
 #include "token_list.h"
 #include <stdarg.h>
+#include <string.h>
 
-
-void test_tokenizer(int a,...)
+void test_tokenizer(int i, ...)
 {
+	char str[14] = "";
+	char a[] = "HOLA que ";
+	char b[] = "%d";
+	char c[] = "%p";
+
+	strcat(strcat(strcat(str, a), b), c);
+
 	int args_arr[] = {10, 20};
     va_list args;
     va_start(args, args_arr);
 
-	/*t_token_list *tokens = tokenize("HOLA que %d tal %p", args);
+	t_token_list *tokens = tokenize(str, args);
 	t_token_node *current = *tokens;
 	start_unit_test();
-	assert_equal_str(current->token->value, "HOLA que ");*/
+	assert_not_null(tokens);
+	assert_equal_str((char *)current->token->value, c);
+	assert_equal_str((char *)current->next->token->value, b);
+	assert_equal_str((char *)current->next->next->token->value, a);
 }
 
 int main(int argc, char const *argv[])
