d540d29cb58790ba3e10b29dcb7157c565ea8bd1
Luis <luiscristiangrigore11@gmail.com>
2025-02-24 15:27:11 +0100

    aa

--- test/tokenizer/test_tokenizer.c
+++ test/tokenizer/test_tokenizer.c
@@ -3,40 +3,43 @@
 #include "token_list.h"
 #include <stdarg.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdarg.h>
 
-void test_tokenizer(void *i, ...)
-{
+void test_tokenizer(int num_args, ...) {
 	//Arrange
 	char a[] = "HOLA que ";
 	char b[] = "%d";
+	char c[] = "%s";
 
 	char str[14] = "";
-	strcat(strcat(str, a), b);
+	strcat(strcat(strcat(str, a), b),c);
 
     va_list args;
-    va_start(args, i);
+    va_start(args, num_args);
+    
+    void *first_arg = va_arg(args, void *);
+    
+    int valor = *(int *)first_arg;
+    
+    printf("El valor del primer argumento es: %d\n", valor);
 
 	//Act
 	t_token_list *tokens = tokenize(str, args);
 	t_token_node *current = *tokens;
-
-	//Assert
+    
+   //Assert
 	start_unit_test();
 	assert_not_null(tokens);
-	int *val = current->token->value;
-	assert_equal_num(*val, 10);
-	assert_equal_str((char *)current->next->token->value, a);
+	printf("%s", va_arg(args, char *));
+	/*assert_equal_str((char *)current->token->value, c);
+	/*assert_equal_num(*((int *)current->next->token->value), 10);
+	assert_equal_str((char *)current->next->next->token->value, a);*/
 
 	va_end(args);
 }
 
-int main(int argc, char const *argv[])
-{
-	int i = 10;
-	int j = 10;
-
-	test_tokenizer(&i,&j);
-	return 0;
+int main() {
+    int valor = 42;
+	char *a = " que tal";
+    test_tokenizer(2, &valor, a); // Ejemplo de llamada con 3 argumentos
+    return 0;
 }
